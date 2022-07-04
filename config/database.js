0f2bc5f249ec82b5d87e969abb570f89b1933875
osvaldoerens <osvaldoerens@gmail.com>
2022-07-04 22:21:46 +0700

    pertemuan kedua sampai insert data

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ const koneksi = mysql.createConnection ({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'db_moovies',
+    database: 'dbmovie',
     multiplestatements: true 
 });
 // koneksi database
